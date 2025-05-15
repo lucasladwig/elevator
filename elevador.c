565cc6d82c2bfc6b0ec8c1c6987d78f145368369
Alexandre Conte <alexandreconte.dev@gmail.com>
2025-05-15 15:48:53 -0300

    valida parametros do arquivo de entrada

--- elevador.c
+++ elevador.c
@@ -1,1 +1,9 @@
+#include <pthread.h>
 // incluir logica dos elevadores
+
+typedef struct
+{
+  int id;
+  int andar_atual;
+  pthread_t thread;
+} Elevador;
--- main.c
+++ main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "controlador.h"
 #include "elevador.h"
 
 #define TRUE 1
 #define FALSE 0
 
-
-int main (int argc, char* argv[]) 
+int parametros_sao_validos(int num_andares, int num_elevadores, int num_chamadas)
 {
+    if (num_andares < 2 || num_elevadores < 2 || num_chamadas < 2)
+    {
+        printf("Erro: todos os parametros devem ser maiores que 2.\n");
+        return FALSE;
+    }
+    return TRUE;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        printf("Uso: %s <num_andares> <num_elevadores> <num_chamadas>\n", argv[0]);
+        return 1;
+    }
+    int num_andares = atoi(argv[1]);
+    int num_elevadores = atoi(argv[2]);
+    int num_chamadas = atoi(argv[3]);
+
+    if (!parametros_sao_validos(num_andares, num_elevadores, num_chamadas))
+        return 1;
+
+    printf("Inicializando simulação com %d andares, %d elevadores e %d chamadas.\n", num_andares, num_elevadores, num_chamadas);
+
     // inserir logica das threads
 }
